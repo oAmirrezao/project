d656cd419c2288bb3d9a0a59cd6b8bb0513e24cf
oAmirrezao <119684561+oAmirrezao@users.noreply.github.com>
2024-01-31 09:48:41 +0330

    some comments were added

--- Project/src/Book.java
+++ Project/src/Book.java
@@ -4,18 +4,26 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import javax.imageio.ImageIO;
+
+// if we use new File then a photo from current device will be read
+// if we use new URL then a photo from web will be load
 public class Book {
+    // define class features
     private User owner;
     private String title;
     private String author;
+    // it is image address
     private String imageName;
     private BufferedImage image;
     private String publisher;
     private Category category;
     private int rate;
     private long price;
+    // list of all books
     public static ArrayList<Book> books = new ArrayList<>();
+    // list of books which can be sold
     public static ArrayList<Book> booksForBuy = new ArrayList<>();
+    // constructor
     public Book(User owner, String title, String author, String imageName, String publisher, long price) {
         this.owner = owner;
         this.title = title;
@@ -36,34 +44,44 @@ public class Book {
         if (owner.role.equalsIgnoreCase("seller"))
             books.add(this);
     }
+    // to string method to define how the class will be show if we print it
     @Override
     public String toString() {
         return "Title : " + this.title + ", Author : " + this.author + ", Publisher : " + this.publisher + ", Price : " + this.price + ", Owner : " + owner.username;
     }
+    // getter to get title name
     public String getTitle() {
         return title;
     }
+    // setter to set title name
     public void setTitle(String title) {
         this.title = title;
     }
+    // getter to get author name
     public String getAuthor() {
         return author;
     }
+    // setter to set author name
     public void setAuthor(String author) {
         this.author = author;
     }
+    // getter to get image address
     public String getImageName() {
         return imageName;
     }
+    // getter to get publisher name
     public String getPublisher() {
         return publisher;
     }
+    // setter to set publisher name
     public void setPublisher(String publisher) {
         this.publisher = publisher;
     }
+    // getter function to get image
     public BufferedImage getImage() {
         return image;
     }
+    // setter function to set image address
     public void setImageName(String imageName) {
         this.imageName = imageName;
         try {
@@ -73,8 +91,10 @@ public class Book {
             System.out.println("Error: " + e);
         }
     }
+    // function to check if an image address is there in current device
     public static boolean validateImageAddress(String imageName) {
         BufferedImage image;
+        // if image address was invalid it will go to catch and returns false
         try {
             image = ImageIO.read(new File(imageName));
             return true;
@@ -82,19 +102,24 @@ public class Book {
             return false;
         }
     }
+    // getter to get average rate of a book
     public int getRate() {
         return rate;
     }
+    // getter to get price of a book
     public long getPrice() {
         return price;
     }
+    // getter to get owner of this book
     public User getOwner() {
         return this.owner;
     }
+    // setter to set owner of this book
     public void setOwner(User owner) {
         this.owner = owner;
     }
 
+    // it  will be used when we want to show a book features
     public void showBook() {
         System.out.println("Image: " + image);
         System.out.println("Title: " + title);
@@ -102,6 +127,7 @@ public class Book {
         System.out.println("Publisher: " + publisher);
     }
 
+    // this will delete a book from book list if the book we want exists
     public static void deleteBook(Book book) {
         if (!books.isEmpty()) {
             for (int i = 0; i < books.size(); i++) {
@@ -112,7 +138,7 @@ public class Book {
             }
         }
     }
-
+    // this will check if a wanted book is availabe in list of books or not
     public static boolean isAvailable(Book book) {
         if (!books.isEmpty()) {
             for (int i = 0; i < books.size(); i++) {

b931bb46e740a3d3100a92d79420c56f1b864c5e
oAmirrezao <119684561+oAmirrezao@users.noreply.github.com>
2024-01-31 09:35:57 +0330

    a comment in Main.java

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
+    // entry point of project
     public static void main(String[] args) {
         SignUpLoginMenu.getSignUpLoginMenu().setVisible(true);
-//        signUpLoginMenu.addUser("amirreza", "Aamir**1@", "inanloo@gmail.com", 912428367, "customer");
-//        signUpLoginMenu.addUser("amirreza", "amire2", "ali@gmail.com", 13454, "seller");
     }
 }

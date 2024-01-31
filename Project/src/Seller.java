ee136e2d88e6a152427051397966a5f65d3070a2
oAmirrezao <119684561+oAmirrezao@users.noreply.github.com>
2024-01-31 10:17:33 +0330

    some comments added

--- Project/src/Seller.java
+++ Project/src/Seller.java
@@ -1,10 +1,16 @@
+// this class inherits from User class
+// so every object of this class
+// is an instance of User
 public class Seller extends User{
     public Seller(String username, String password, String email, String phoneNumber) {
+        // super class constructor must be first part in the constructor
+        // because thisi class inherits from User class
         super(username, password, email, phoneNumber);
         this.role = "seller";
         currentUser = this;
     }
 
+    // function which when we add a book to user books will execute
     public void addBook(String owner, String title, String author, String imageName, String publisher, long price) {
         this.books.add(new Book(this, title, author, imageName, publisher, price));
     }
